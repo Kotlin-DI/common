f07a6a2b4cce326ef872f6c07980fda8f8db7818
Ivan Molodtsov <ivan.molodtsov@lunatech.nl>
2022-09-23 13:24:19 +0200

    refactor

--- src/main/kotlin/com/github/kotlin_di/common/annotations/IDependency.kt
+++ src/main/kotlin/com/github/kotlin_di/common/annotations/IDependency.kt
@@ -1,5 +1,11 @@
 package com.github.kotlin_di.common.annotations
 
+import kotlin.reflect.KClass
+
+/** Marks class or function as Dependency definition
+ * @param key name for the dependency to be registered as
+ * @param returns return type of Dependency.invoke() method
+ */
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
-annotation class IDependency(val key: String)
+annotation class IDependency(val key: String, val returns: KClass<out Any>)

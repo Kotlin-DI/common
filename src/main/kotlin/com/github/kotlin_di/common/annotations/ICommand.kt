18824d24b8242db19dddb206c72f2c0898ba5cab
Ivan Molodtsov <ivan.molodtsov@lunatech.nl>
2022-10-07 14:11:39 +0200

    refactor

--- src/main/kotlin/com/github/kotlin_di/common/annotations/ICommand.kt
+++ src/main/kotlin/com/github/kotlin_di/common/annotations/ICommand.kt
@@ -1,5 +1,5 @@
 package com.github.kotlin_di.common.annotations
 
 @Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.FUNCTION)
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
 annotation class ICommand

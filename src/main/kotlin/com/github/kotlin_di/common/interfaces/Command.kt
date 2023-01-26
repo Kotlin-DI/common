5e47fa3967052377fa04be5602ef942c0807d39d
Ivan Molodtsov <ivan.molodtsov@lunatech.nl>
2023-01-26 15:34:57 +0100

    refactor

--- src/main/kotlin/com/github/kotlin_di/common/interfaces/Command.kt
+++ src/main/kotlin/com/github/kotlin_di/common/interfaces/Command.kt
@@ -2,7 +2,7 @@ package com.github.kotlin_di.common.interfaces
 
 import com.github.kotlin_di.common.errors.CommandExecutionError
 
-fun interface Command: () -> Unit {
+fun interface Command : () -> Unit {
     @Throws(CommandExecutionError::class)
     override fun invoke()
 }
